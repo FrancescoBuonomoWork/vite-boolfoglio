656ed28f5227479ec1f72862f3a929e074b5f046
FrancescoBuonomo <buonomofrancescow@gmail.com>
2024-01-15 16:38:02 +0100

    fix project tpye wit ternar operator

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -20,8 +20,10 @@ export default{
 
 <template>
     <div class="card">
+
        <h2 class="card-title">{{ project.name }}</h2>
-       <p v-if="project.type.name">{{ project.type.name }}</p>
+       <p >{{project.type ? project.type.name : '-' }}</p>
+       <!-- <p v-if="project.type">{{ project.type.name  }}</p> -->
        <ul v-if="project.technologies">
         <li v-for="tech in project.technologies" :key="tech.id">{{ tech.name }}</li>
        </ul>
