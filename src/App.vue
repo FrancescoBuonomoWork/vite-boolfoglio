5d771c5e2c1957a4b23ca25b97d780a80c7ed5ae
FrancescoBuonomo <buonomofrancescow@gmail.com>
2024-01-15 15:25:22 +0100

    add FetchProjects method

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+export default{
+  data(){
+    return{
+      projects: [],
+      BASE_URL :'http://localhost:8000/api'
+    }
+  },
+  methods:{
+
+    FetchProjects(){
+
+      axios.get(`${this.BASE_URL}/projects`)
+      .then((res)=>{
+        console.log(res)
+        this.projects = res.data.results
+      })
+     
+    }
+  },
+  created(){
+    this.FetchProjects()
+  }
+}
 
 
 </script>
 
 <template>
+
+  <div v-for="project in projects" :key="project.id">
+    {{ project.name }}
+  </div>
  
 </template>
 
